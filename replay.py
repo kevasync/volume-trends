f521f1429ecbe8b3fe4f7a1c6ed981e8ae3c929a
kevin.tinn <kevin.tinn@asynchrony.com>
2018-03-04 22:33:56 -0700

    baby replay is born

---
+++ replay.py
@@ -0,0 +1,30 @@
+from util import *
+
+configPath = getConfigPathFromArgs(sys.argv)
+config = getConfig(configPath)
+intervals = config.get(intervalsConfigKey)
+replayTable = getCouchDbTable('volume-ratio')
+
+symbols = sys.argv[1].split(',')
+start = int(sys.argv[2])
+end = int(sys.argv[3])
+skipInterval = int(sys.argv[4])
+
+idx = start
+while idx <= end:
+	try:
+		data = replayTable['{}'.format(idx)]
+		for s in symbols:
+			if s in data:
+				symbolData = data[s]
+				for i in intervals:
+					k = '{}'.format(i)
+					if k in symbolData:
+						val = symbolData[k]
+						if val != '?': 
+							print('{} - {} - {}: {}'.format(idx, s, i, float(val)))
+							
+	except couchdb.http.ResourceNotFound:
+		print('data not found: {}'.format(idx))
+	idx += skipInterval
+
